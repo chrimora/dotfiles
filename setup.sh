2cff7cd756fb955d6cbd3c9d8962d20a13c94e75
chrimora <chris.d.moran@live.co.uk>
2023-07-10 21:18:59 +0100

    setup script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function linkdotfile {
+  file="$1"
+
+  ln -s ~/dotfiles/$file ~/$file
+}
+
+# Editors
+linkdotfile .vimrc
+
+linkdotfile .config/helix/config.toml
+linkdotfile .config/helix/languages.toml
